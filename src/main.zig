1666d7ab812133dfa9e5028be959de987a66daaa
Vincent Rischmann <vincent@rischmann.fr>
2020-03-05 00:06:46 +0100

    add readWordList

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,33 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const WordList = struct {
+    language: []const u8,
+    words: [2047][]const u8,
+};
+
+fn readWordList(comptime language: []const u8, comptime data: []const u8) anyerror!WordList {
+    var words: [2047][]const u8 = undefined;
+
+    var iter = std.mem.tokenize(data, "\n");
+    var i: usize = 0;
+    while (iter.next()) |line| {
+        words[i] = line;
+        i += 1;
+    }
+
+    return WordList{
+        .language = language,
+        .words = words,
+    };
 }
 
-test "basic add functionality" {
-    testing.expect(add(3, 7) == 10);
+const data_english = @embedFile("wordlist_english.txt");
+
+test "read all words" {
+    const english = try readWordList("english", data_english);
+
+    for (english.words[0..2]) |word| {
+        std.debug.warn("word: {}\n", .{word});
+    }
 }
