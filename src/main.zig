c086263596ee1cf7ef6e59e83338b98093ec4237
Vincent Rischmann <vincent@rischmann.fr>
2020-03-05 00:16:41 +0100

    fix tests

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,12 @@ const testing = std.testing;
 
 const WordList = struct {
     language: []const u8,
-    words: [2047][]const u8,
+    words: [2048][]const u8,
 };
 
+/// Creates a WordList from the input data for the language provided.
 fn readWordList(comptime language: []const u8, comptime data: []const u8) anyerror!WordList {
-    var words: [2047][]const u8 = undefined;
+    var words: [2048][]const u8 = undefined;
 
     var iter = std.mem.tokenize(data, "\n");
     var i: usize = 0;
@@ -27,7 +28,6 @@ const data_english = @embedFile("wordlist_english.txt");
 test "read all words" {
     const english = try readWordList("english", data_english);
 
-    for (english.words[0..2]) |word| {
-        std.debug.warn("word: {}\n", .{word});
-    }
+    testing.expect(std.mem.eql(u8, english.language, "english"));
+    testing.expect(english.words.len == 2048);
 }
